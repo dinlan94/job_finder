e8ac52f4b13f16bded83bb12b04718f027046bc2
dinlan94 <fajarudin96@gmail.com>
2023-02-23 13:43:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# job_finder
+ website for job finder
---
+++ src/Components/SearchDiv/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const Search = () => {
+  return (
+    <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+      <form action="">
+
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px]
+        bg-white p-5 shadow-lg shadow-greyIsh-700'>
+
+          <div className='flex gap-2 items-center'>
+            <AiOutlineSearch className='text-[25px] icon' />
+            <input type="text" className='bg-transparent text-gray-900 focus:outline-none w-[100%]' placeholder='Search job here...' />
+          </div>
+
+        </div>
+
+      </form>
+    </div>
+  );
+};
+
+export default Search;
